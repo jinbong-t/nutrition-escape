7008a988691ac8ca5b7bb0673dd213fb57bd750d
jinbong <salasa01@hanmail.net>
2026-07-22 10:52:02 +0900

    뼈다귀 게임 저해인자 피격 시 화면 전체 흔들림 현상 수정 (캐릭터만 흔들리도록 변경)

--- app/app.js
+++ app/app.js
@@ -954,9 +954,13 @@ function handleBoneCollision(item) {
         boneHealth -= 30;
         updateBoneUI();
         
-        // 화면 흔들림 및 붉은 섬광 효과
-        container.classList.add('shake', 'flash-red');
-        setTimeout(() => container.classList.remove('shake', 'flash-red'), 500);
+        // 뼈다귀 캐릭터 흔들림 및 컨테이너 붉은 섬광 효과
+        container.classList.add('flash-red');
+        player.classList.add('shake-player');
+        setTimeout(() => {
+            container.classList.remove('flash-red');
+            player.classList.remove('shake-player');
+        }, 500);
         
         const floatText = document.createElement('div');
         floatText.className = 'floating-text';
